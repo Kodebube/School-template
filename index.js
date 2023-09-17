5461e10d79abab06f9f1f6a7bf54ac613593c04c
Kodebube <137894304+Kodebube@users.noreply.github.com>
2023-09-17 19:06:51 +0100

    Update index.js
    comot error

--- index.js
+++ index.js
@@ -44,3 +44,60 @@ document.getElementById('hadto').onclick = function (){
   document.getElementById('hadto').style.animation = 'bling'
   document.getElementById('hadto').style.marginLeft = '80%'
 }
+
+
+
+//testimonial slide
+
+document.getElementById('frr').style.opacity = '1'
+document.getElementById('imgg').innerHTML = '<img src="img/teen2.jpg" alt="ttmn1">'
+document.getElementById('tname').innerHTML = 'Ajuwaghan Praise'
+document.getElementById('ttype').innerHTML = 'Ex-student'
+document.getElementById('ttmn').innerHTML = 'I love this school! They have really carved me in to the person I am today! Kudos to [name of. School]! I owe you guys one.'
+
+document.getElementById('oneee').onclick = function (){
+    document.getElementById('frr').style.opacity = '1'
+    document.getElementById('imgg').innerHTML = '<img src="img/teen2.jpg" alt="ttmn1">'
+    document.getElementById('tname').innerHTML = 'Ajuwaghan Praise'
+    document.getElementById('ttype').innerHTML = 'Ex-student'
+    document.getElementById('ttmn').innerHTML = 'I love this school! They have really carved me in to the person I am today! Kudos to [name of. School]! I owe you guys one.'
+    
+    document.getElementById('scd').style.opacity = '.5'
+    document.getElementById('fth').style.opacity = '.5'
+    document.getElementById('trd').style.opacity = '.5'
+}
+
+document.getElementById('twooo').onclick = function (){
+    document.getElementById('scd').style.opacity = '1'
+    document.getElementById('imgg').innerHTML = '<img src="img/teen1.jpg" alt="ttmn1">'
+    document.getElementById('tname').innerHTML = 'Dele Badmus'
+    document.getElementById('ttype').innerHTML = 'Former Teacher'
+    document.getElementById('ttmn').innerHTML = '[name of. School] is a place were leaders are built, and memories are made. I\m so glad that I was a part of these memories.'
+
+    document.getElementById('frr').style.opacity = '.5'
+    document.getElementById('fth').style.opacity = '.5'
+    document.getElementById('trd').style.opacity = '.5'
+}
+document.getElementById('threee').onclick = function (){
+    document.getElementById('trd').style.opacity = '1'
+    document.getElementById('imgg').innerHTML = '<img src="img/teen3.jpg" alt="ttmn1">'
+    document.getElementById('tname').innerHTML = 'Chris Jekulski'
+    document.getElementById('ttype').innerHTML = 'Set 2020/21 Valedictorian'
+    document.getElementById('ttmn').innerHTML = 'When I was in JSS3, I didn\'t really understand Mathematics until, I transferred to [name of. School]. I have been having A\'s ever since!'
+    
+    document.getElementById('frr').style.opacity = '.5'
+    document.getElementById('scd').style.opacity = '.5'
+    document.getElementById('fth').style.opacity = '.5'
+}
+document.getElementById('fourrr').onclick = function (){
+    document.getElementById('fth').style.opacity = '1'
+    document.getElementById('imgg').innerHTML = '<img src="img/teen4.jpg" alt="ttmn1">'
+    document.getElementById('tname').innerHTML = 'Deborah Dale'
+    document.getElementById('ttype').innerHTML = 'Present Head Girl'
+    document.getElementById('ttmn').innerHTML = 'What I like best about this school is its computer lab. It\'s air-conditioned, It\'s quiet. It is everything I ever wished for!'
+    
+    document.getElementById('frr').style.opacity = '.5'
+    document.getElementById('scd').style.opacity = '.5'
+    document.getElementById('trd').style.opacity = '.5'
+}
+//testimonial slide ends
